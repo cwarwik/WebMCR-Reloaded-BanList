12e66b95d8cbf2fa9a7e12b3c57f6567d2cf5e43
cwarwik <93426483+cwarwik@users.noreply.github.com>
2021-11-01 20:17:44 +0300

    Update banlist.php

--- upload/modules/banlist.php
+++ upload/modules/banlist.php
@@ -14,87 +14,94 @@ class module{
 		$this->lng		= $core->lng_m;
 
 		$bc = array(
-			$this->lng['mod_name'] => BASE_URL."?mode=clans"
+			$this->lng['mod_name'] => BASE_URL."?mode=banlist"
 		);
 		
 		$this->core->bc = $this->core->gen_bc($bc);
     }
+    
+    private function banlist_array() {
+        ob_start();
 
-	// Топ кланов
-	private function clansTop() {
-		ob_start();
-
-		$start = $this->core->pagination($this->cfg_m['PAGINATION'], 0, 0);
+        $start = $this->core->pagination($this->cfg_m['PAGINATION'], 0, 0);
         $end = $this->cfg_m['PAGINATION'];
 
-		$query = $this->db->query("SELECT *, COUNT(*) AS tagcount, {$this->cfg_m['MOD_SETTING']['TABLE_CLANS']}.name AS nclans FROM `{$this->cfg_m['MOD_SETTING']['TABLE_CLANS']}` LEFT JOIN `{$this->cfg_m['MOD_SETTING']['TABLE_PLAYERS']}` ON {$this->cfg_m['MOD_SETTING']['TABLE_CLANS']}.tag = {$this->cfg_m['MOD_SETTING']['TABLE_PLAYERS']}.tag LIMIT $start, $end");
+        $query = $this->db->query("SELECT `{$this->cfg_m['MOD_SETTING']['ID']}`, `{$this->cfg_m['MOD_SETTING']['LOGIN']}`, `{$this->cfg_m['MOD_SETTING']['ADMIN']}`,
+										`{$this->cfg_m['MOD_SETTING']['REASON']}`, `{$this->cfg_m['MOD_SETTING']['TIME_START']}`, `{$this->cfg_m['MOD_SETTING']['TIME_END']}`,
+										`{$this->cfg_m['MOD_SETTING']['TYPE']}`
+									FROM `{$this->cfg_m['MOD_SETTING']['TABLE']}`
+									ORDER BY `{$this->cfg_m['MOD_SETTING']['ID']}` DESC
+									LIMIT $start,$end");
 
-		if (!$query || $this->db->num_rows($query)<=0) {
-			echo $this->core->sp(MCR_THEME_MOD."clans/clans-none.html");
-			return ob_get_clean();
-		}
+        if(!$query || $this->db->num_rows($query)<=0){ echo $this->core->sp(MCR_THEME_MOD."banlist/banlist-none.html"); return ob_get_clean(); } // Check returned result
 
-		while ($ar = $this->db->fetch_assoc($query)) {
+        while($ar = $this->db->fetch_assoc($query)){
+
+            $type = $this->db->HSC($ar[$this->cfg_m['MOD_SETTING']['TYPE']]);
+            $timeend = $this->db->HSC($ar[$this->cfg_m['MOD_SETTING']['TIME_END']]);
+            
+            switch($type) {
+                case 'BAN': $type = $this->lng['type_ban'];
+                break;
+                case 'TEMP_BAN': $type = $this->lng['type_tempban'];
+                break;
+                case 'MUTE': $type = $this->lng['type_mute'];
+                break;
+                case 'TEMP_MUTE': $type = $this->lng['type_tempmute'];
+                break;
+                case 'WARNING': $type = $this->lng['type_warn'];
+                break;
+                case 'TEMP_WARNING': $type = $this->lng['type_tempwarn'];
+                break;
+            }
+
+            ($timeend == -1) ? $timeend = $this->lng['time_pernament'] : $timeend = date("d.m.Y в H:i", $ar[$this->cfg_m['MOD_SETTING']['TIME_END']]/1000);
+
 			$data = array(
-				'ID' => intval($ar['id']),
-				'TAG' => $this->db->HSC($ar['tag']),
-				'NAME' => $this->db->HSC($ar['nclans']),
-				'COUNT' => $this->db->HSC($ar['tagcount']),
-				'BALANCE' => $this->db->HSC($ar['balance']),
-			);
+				'ID' => intval($ar[$this->cfg_m['MOD_SETTING']['ID']]),
+				'LOGIN' => $this->db->HSC($ar[$this->cfg_m['MOD_SETTING']['LOGIN']]),
+				'ADMIN' => $this->db->HSC($ar[$this->cfg_m['MOD_SETTING']['ADMIN']]),
+				'REASON' => ($this->db->HSC($ar[$this->cfg_m['MOD_SETTING']['REASON']]) == 'none') ? $this->lng['noreason'] : $this->db->HSC($ar[$this->cfg_m['MOD_SETTING']['REASON']]),
+				'TIME_START' => date("d.m.Y в H:i", $ar[$this->cfg_m['MOD_SETTING']['TIME_START']]/1000),
+				'TIME_END' => $timeend,
+				'TYPE' => $type
+            );
+            
+			echo $this->core->sp(MCR_THEME_MOD."banlist/banlist-users.html", $data);
+        }
 
-			echo $this->core->sp(MCR_THEME_MOD."clans/clans-list.html", $data);
-		}
+        return ob_get_clean();
+    }
 
-		return ob_get_clean();
-	}
-
-	// Топ игроков
-	private function playersTop() {
-		ob_start();
-
-		$start = $this->core->pagination($this->cfg_m['PAGINATION'], 0, 0);
-        $end = $this->cfg_m['PAGINATION'];
-
-		$query = $this->db->query("SELECT * FROM `{$this->cfg_m['MOD_SETTING']['TABLE_PLAYERS']}` ORDER BY `id` DESC LIMIT $start, $end");
-
-		if (!$query || $this->db->num_rows($query)<=0) {
-			echo $this->core->sp(MCR_THEME_MOD."clans/clans-none.html");
-			return ob_get_clean();
-		}
-
-		while ($ar = $this->db->fetch_assoc($query)) {
-			$data = array(
-				'ID' => intval($ar['id']),
-				'TAG' => $this->db->HSC($ar['tag']),
-				'NAME' => $this->db->HSC($ar['name']),
-				'KILL' => $this->db->HSC($ar['neutral_kills'] + $ar['rival_kills'] + $ar['civilian_kills'] + $ar['ally_kills']),
-				'DEATHS' => $this->db->HSC($ar['deaths']),
-				'LAST_SEEN' => date("d.m.Y в H:i", $ar['last_seen']/1000),
-			);
-
-			echo $this->core->sp(MCR_THEME_MOD."clans/clans-players.html", $data);
-		}
-		return ob_get_clean();
-	}
+    private function banlist_stutus() {
+        ob_start();
+        $query = $this->db->query("SELECT * FROM `{$this->cfg_m['MOD_SETTING']['TABLE']}` WHERE `{$this->cfg_m['MOD_SETTING']['LOGIN']}`='".$this->user->login."'");
+		
+        if ($this->db->num_rows($query) > 0) {
+            echo $this->core->sp(MCR_THEME_MOD."banlist/alerts/yesban.html");
+        }else {
+            echo $this->core->sp(MCR_THEME_MOD."banlist/alerts/noban.html");
+        }
+        return ob_get_clean();
+    }
 
 	public function content(){
 
-        $page	= "?mode=clans&pid=";
-        $query = $this->db->query("SELECT COUNT(*) FROM `{$this->cfg_m['MOD_SETTING']['TABLE_CLANS']}`");
-        
+        $page = "?mode=banlist&pid=";
+        $query = $this->db->query("SELECT COUNT(*) FROM `{$this->cfg_m['MOD_SETTING']['TABLE']}`");
+
         if(!$query){exit("SQL Error");}
 
         $ar = $this->db->fetch_array($query);
         
         $data = array(
+            "STATUS" => $this->banlist_stutus(),
 			"PAGINATION" => $this->core->pagination($this->cfg_m['PAGINATION'], $page, $ar[0]),
-            "TOP" => $this->clansTop(),
-			"PLAYERS" => $this->playersTop(),
+            "CONTENT" => $this->banlist_array(),
         );
         
-        $this->core->header = $this->core->sp(MCR_THEME_MOD."clans/header.html");
-        return $this->core->sp(MCR_THEME_MOD."clans/clans-full.html", $data);
+        $this->core->header = $this->core->sp(MCR_THEME_MOD."banlist/header.html");
+        return $this->core->sp(MCR_THEME_MOD."banlist/banlist-full.html", $data);
         
 	}
 }
